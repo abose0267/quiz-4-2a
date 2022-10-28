57fd32888f8b558a1e36ae9fa51bce135f438a99
Arka Bose <arkaprabhobose@gmail.com>
2022-10-28 00:02:26 +0000

    push

--- handshake.cpp
+++ handshake.cpp
@@ -1,28 +1,60 @@
+#include <condition_variable>
 #include <iostream>
+#include <mutex>
+#include <thread>
+#include <vector>
+
 // include additional necessary headers
 
-void query(/*add necessary parameters*/) {
-    // Should print: the print number (starting from 0), "SYN", and the three dots "..."
+void query(std::mutex *m, int no) {
+  // Should print: the print number (starting from 0), "SYN", and the three dots
+  std::unique_lock<std::mutex> l(*m);
+
+  std::cout << "[" << no << "]"
+            << " SYN ...";
+
+  l.unlock();
+  // "..."
 }
 
-void response(/*add necessary parameters*/) {
-    // Should print "ACK"
+void response(std::mutex *m) {
+  // Should print "ACK"
+  std::unique_lock<std::mutex> l(*m);
+
+  std::cout << " ACL\n";
+  l.unlock();
 }
 
-int main(int argc, char** argv) {
-    if (argc < 2) {
-        printf("Usage: ./handshake <count>\n");
-        exit(1);
-    }
+int main(int argc, char **argv) {
 
-    /**
-     * Steps to follow:
-     * 1. Get the "count" from cmd args
-     * 2. Create necessary synchronization primitive(s)
-     * 3. Create two threads, one for "SYN" and the other for "ACK"
-     * 4. Provide the threads with necessary args
-     * 5. Update the "query" and "response" functions to synchronize the output
-    */
-   
-    return 0;
+  if (argc < 2) {
+    printf("Usage: ./handshake <count>\n");
+    exit(1);
+  }
+
+  int count = atoi(argv[1]);
+
+  std::mutex m;
+  std::vector<std::thread> tVec1;
+  std::vector<std::thread> tVec2;
+  for (int i = 0; i < count; i++) {
+
+    std::thread t1 = std::thread(query, &m, i);
+    std::thread t2 = std::thread(response, &m);
+
+    t1.join();
+    t2.join();
+  }
+
+  /**
+   * Steps to follow:
+   * 1. Get the "count" from cmd args
+
+   * 2. Create necessary synchronization primitive(s)
+   * 3. Create two threads, one for "SYN" and the other for "ACK"
+   * 4. Provide the threads with necessary args
+   * 5. Update the "query" and "response" functions to synchronize the output
+  */
+
+  return 0;
 }
